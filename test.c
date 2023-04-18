70249f883d8cb8290406e026cabb836c6a6359c4
KiraAme <77437133+KiraAme@users.noreply.github.com>
2023-04-18 14:37:59 +0200

    Create test.c

---
+++ test.c
@@ -0,0 +1,42 @@
+#include "libGameRGR2.h"
+
+typedef struct{
+  int x;
+  int y;
+} Model;
+
+
+
+
+void Init (void* pUSerData, Screen* pScreen){
+  
+}
+void Event (void* pUserData, Screen* pScreen, Event* pEvt){
+  
+}
+int Update (void* pUserData, Screen* pScreen, unsigned long deltaTime){
+  
+  return 0;
+}
+void Draw (void* pUSerData, Screen* pScreen){
+  
+}
+void Finish (void* pUserData){
+  
+}
+
+
+
+
+
+
+int main() {
+  
+  
+  
+  
+  
+  
+  
+  return 0; 
+}
