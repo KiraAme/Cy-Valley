e480dd9adc4a6352587e15a7474fbceb7315e131
KiraAme <77437133+KiraAme@users.noreply.github.com>
2023-04-18 15:07:13 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,4 +1,4 @@
-#include "libGameRGR2.h"
+#include "inc/libGameRGR2.h"
 
 typedef struct{
   int x;
@@ -8,17 +8,17 @@ typedef struct{
 
 
 
-void Init (void* pUSerData, Screen* pScreen){
-  
+void Init (void* pUserData, Screen* pScreen){
+  Model* pModel = (*Model)pUserData;
 }
 void Event (void* pUserData, Screen* pScreen, Event* pEvt){
-  
+  Model* pModel = (*Model)pUserData;
 }
 int Update (void* pUserData, Screen* pScreen, unsigned long deltaTime){
   
   return 0;
 }
-void Draw (void* pUSerData, Screen* pScreen){
+void Draw (void* pUserData, Screen* pScreen){
   
 }
 void Finish (void* pUserData){
@@ -31,10 +31,16 @@ void Finish (void* pUserData){
 
 
 int main() {
+  Model model;
+  Callbacks cb;
+  cb->cbInit= Init;        
+  cb->cbEvent= Event;        
+  cb->cbUpdate= Update;
+  cb->cbDraw= Draw;
+  cb->cbFinish= Finish;
   
   
-  
-  
+  createGame(10, 30, &model, &cb, 0);
   
   
   
