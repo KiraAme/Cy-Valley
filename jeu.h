a6d1a41b3c4249a6372950bacd61f0ea9d684d20
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2023-04-27 21:19:52 +0200

    Add files via upload

---
+++ jeu.h
@@ -0,0 +1,17 @@
+#include "inc/libGameRGR2.h"
+#include <signal.h>
+#include <unistd.h>
+
+typedef struct{
+  int x;
+  int y;
+  int arrow_position;
+  int game_status;
+} Model;
+
+void menu(Screen* pScreen, Event* pEvt, Model* pModel);
+
+void event(void* pUserData, Screen* pScreen, Event* pEvt);
+int update(void* pUserData, Screen* pScreen, unsigned long deltaTime);
+void draw(void* pUserData, Screen* pScreen);
+void finish(void* pUserData);
